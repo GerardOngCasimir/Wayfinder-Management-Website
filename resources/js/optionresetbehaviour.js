d76dcd56d6efb1e6c96900093857ab6a86da3aa1
GerardOngCasimir <060764@apac.nttdata.com>
2024-04-16 11:24:48 +0800

    Separated script behaviour from index

--- resources/js/optionresetbehaviour.js
+++ resources/js/optionresetbehaviour.js
@@ -1,45 +1,43 @@
-<script>
-    function enableQuestion2() {
+function enableQuestion2() {
     var question1Value = document.getElementById("facility").value;
     var question2Select = document.getElementById("level");
     var question2Div = document.getElementById("question2-div");
 
     if (question1Value) {
-      question2Select.disabled = false;
-      question2Div.style.opacity = 1; // Make the second question visible
+        question2Select.disabled = false;
+        question2Div.style.opacity = 1; // Make the second question visible
     } else {
-      question2Select.disabled = true;
-      question2Div.style.opacity = 0.5; // Dim the second question
-      document.getElementById("level").value = "";
+        question2Select.disabled = true;
+        question2Div.style.opacity = 0.5; // Dim the second question
+        document.getElementById("level").value = "";
     }
 
     // Reset question 3 if question 2 is disabled
     if (question2Select.disabled) {
-      disableQuestion3();
+        disableQuestion3();
     }
-  }
+}
 
-    function enableQuestion3() {
+function enableQuestion3() {
     var question2Value = document.getElementById("level").value;
     var question3Select = document.getElementById("POI");
     var question3Div = document.getElementById("question3-div");
 
     if (question2Value) {
-      question3Select.disabled = false;
-      question3Div.style.opacity = 1; // Make the second question visible
+        question3Select.disabled = false;
+        question3Div.style.opacity = 1; // Make the second question visible
     } else {
-      question3Select.disabled = true;
-      question3Div.style.opacity = 0.5; // Dim the second question
-      document.getElementById("POI").value = "";
+        question3Select.disabled = true;
+        question3Div.style.opacity = 0.5; // Dim the second question
+        document.getElementById("POI").value = "";
     }
-  }
+}
 
-    function disableQuestion3() {
+function disableQuestion3() {
     var question3Select = document.getElementById("POI");
     var question3Div = document.getElementById("question3-div");
 
     question3Select.disabled = true;
     question3Div.style.opacity = 0.5; // Dim the third question
     document.getElementById("POI").value = "";
-  }
-</script>
+}
