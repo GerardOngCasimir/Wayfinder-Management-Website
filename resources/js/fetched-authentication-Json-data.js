b2f344cc5cb1ccef65c7026c2bf82bfc00e387e2
GerardOngCasimir <060764@apac.nttdata.com>
2024-04-26 21:14:53 +0800

    Cleaned up Authentication code

--- resources/js/fetched-authentication-Json-data.js
+++ resources/js/fetched-authentication-Json-data.js
@@ -14,17 +14,9 @@
         const authUrl = 'https://ap-southeast-1.aws.services.cloud.mongodb.com/api/client/v2.0/app/data-btthb/auth/providers/local-userpass/login';
 
         const requestData = {
-            "username": "060764@apac.nttdata.com",
-            "password": "Gerard821207"
+            "username": username,
+            "password": password
         };
-
-        if (username === requestData.username && password === requestData.password) {
-            // If inputted username and password match, open the website
-            window.open('index.html', '_blank');
-        } else {
-            // If inputted username and password do not match, show an alert
-            alert('Username or password is incorrect. Please try again.');
-        }
 
         return new Promise((resolve, reject) => {
             fetch(authUrl, {
@@ -42,13 +34,14 @@
                 })
                 .then(data => {
                     console.log('Printing successful JSON Data: ' + JSON.stringify(data));
-                    // Check if authentication was successful
+
+                    window.location.href = 'index.html';
 
                 })
                 .catch(error => {
                     console.error('There was a problem with your fetch operation:', error);
-                    // Reject the promise with the error
-                    reject(error);
+                    
+                    alert('Username or password is incorrect. Please try again.');
                 });
         });
     }
