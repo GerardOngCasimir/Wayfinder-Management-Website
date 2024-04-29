7d6ede34382dfe4c8977f7705a9c04b77ad8a665
GerardOngCasimir <060764@apac.nttdata.com>
2024-04-29 14:16:19 +0800

    Fixed access token authorization

--- resources/js/fetched-Json-data.js
+++ resources/js/fetched-Json-data.js
@@ -4,141 +4,98 @@
 //TODO: Create proxy server and forward all requests to proxy server instead for increased security and shortened code
 function fetchInstitutionData()
 {
-    const authUrl = 'https://services.cloud.mongodb.com/api/client/v2.0/app/data-btthb/auth/providers/anon-user/login';
+    // Retrieve the access token from localStorage
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) {
+        console.error('Access token not found.');
+        return;
+    }
 
-    //Fetch authentication token to authenticate users
-    //Temporarily allowing anon user login, not secure
-    //Migrate to a more secure authentication method in the near future
-    fetch(authUrl,
-        {
-            method: 'POST',
-            headers:
-                {
-                    'Content-Type': 'application/json'
-                }
-        })
-        .then(response => response.json())
-        .then(authData =>
-        {
-            //Pass access token to data request header to remove CORS policy on browsers
-            const accessToken = authData.access_token;
-            const dataUrl = 'https://ap-southeast-1.aws.data.mongodb-api.com/app/data-btthb/endpoint/data/v1/action/find';
-            const requestData =
-                {
-                    "collection": "Institutions",
-                    "database": "WayfinderManagement",
-                    "dataSource": "Dev-WayfinderManagement"
-                };
+    const dataUrl = 'https://ap-southeast-1.aws.data.mongodb-api.com/app/data-btthb/endpoint/data/v1/action/find';
 
-            return fetch(dataUrl,
-                {
-                    method: 'POST',
-                    headers:
-                        {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${accessToken}`
-                        },
-                    body: JSON.stringify(requestData),
-                });
-        })
-        .then(response =>
+    const requestData =
         {
-            if (!response.ok)
+            "collection": "Institutions",
+            "database": "WayfinderManagement",
+            "dataSource": "Dev-WayfinderManagement"
+        };
+
+    fetch(dataUrl, {
+        method: 'POST',
+        headers:
             {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data =>
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+            },
+        body: JSON.stringify(requestData),
+    })
+        .then(response =>
+    {
+        if (!response.ok)
         {
-            console.log('Printing successful JSON Data: ' + JSON.stringify(data));
-            populateDropdowns(data);
-            
-            //const institutions = JSON.parse(JSON.stringify(data)).documents[1];
-        })
-        .catch(error =>
-        {
-            console.error('There was a problem with your fetch operation:', error);
-        });
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data =>
+    {
+        //console.log('Printing successful JSON Data: ' + JSON.stringify(data));
+        populateDropdowns(data);
+    })
+    .catch(error =>
+    {
+        console.error('There was a problem with your fetch operation:', error);
+    });
 }
 
 function fetchRouteData()
 {
-    const authUrl = 'https://services.cloud.mongodb.com/api/client/v2.0/app/data-btthb/auth/providers/anon-user/login';
+    // Retrieve the access token from localStorage
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) {
+        console.error('Access token not found.');
+        return;
+    }
 
-    //Fetch authentication token to authenticate users
-    //Temporarily allowing anon user login, not secure
-    //Migrate to a more secure authentication method in the near future
-    fetch(authUrl,
-        {
-            method: 'POST',
-            headers:
-                {
-                    'Content-Type': 'application/json'
-                }
-        })
-        .then(response => response.json())
-        .then(authData =>
-        {
-            //Pass access token to data request header to remove CORS policy on browsers
-            const accessToken = authData.access_token;
-            console.log("Access token:" + accessToken);
-            const dataUrl = 'https://ap-southeast-1.aws.data.mongodb-api.com/app/data-btthb/endpoint/returnTopUserStatistics';
-            // const requestData =
-            //     {
-            //         "collection": "Institutions",
-            //         "database": "WayfinderManagement",
-            //         "dataSource": "Dev-WayfinderManagement"
-            //     };
+    const dataUrl = 'https://ap-southeast-1.aws.data.mongodb-api.com/app/data-btthb/endpoint/returnTopUserStatistics';
 
-            return fetch(dataUrl,
-                {
-                    method: 'POST',
-                    headers:
-                        {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${accessToken}`
-                        },
-                    // body: JSON.stringify(requestData),
-                });
-        })
-        .then(response =>
-        {
-            if (!response.ok)
+    fetch(dataUrl, {
+        method: 'POST',
+        headers:
             {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+            },
+        // body: JSON.stringify(requestData),
         })
-        .then(data =>
+    .then(response =>
+    {
+        if (!response.ok)
         {
-            console.log('Printing successful JSON Data: ' + JSON.stringify(data));
-            populateRoutes(data);
-        })
-        .catch(error =>
-        {
-            console.error('There was a problem with your fetch operation:', error);
-        });
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data =>
+    {
+        //console.log('Printing successful JSON Data: ' + JSON.stringify(data));
+        populateRoutes(data);
+    })
+    .catch(error =>
+    {
+        console.error('There was a problem with your fetch operation:', error);
+    });
 }
 
 async function fetchPOINameData(institutionName, latitude, longitude)
 {
     try {
-        const authUrl = 'https://services.cloud.mongodb.com/api/client/v2.0/app/data-btthb/auth/providers/anon-user/login';
-
-        const authResponse = await fetch(authUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (!authResponse.ok) {
-            throw new Error('Failed to fetch authentication token');
+        // Retrieve the access token from localStorage
+        const accessToken = localStorage.getItem('accessToken');
+        if (!accessToken) {
+            console.error('Access token not found.');
+            return;
         }
-
-        const authData = await authResponse.json();
-        const accessToken = authData.access_token;
 
         const dataUrl = 'https://ap-southeast-1.aws.data.mongodb-api.com/app/data-btthb/endpoint/findAndReturnPointOfInterest';
         const requestData = {
--- resources/js/fetched-authentication-Json-data.js
+++ resources/js/fetched-authentication-Json-data.js
@@ -11,38 +11,39 @@
     });
 
     function fetchAuthenticationLoginData(username, password) {
-        const authUrl = 'https://ap-southeast-1.aws.services.cloud.mongodb.com/api/client/v2.0/app/data-btthb/auth/providers/local-userpass/login';
+        const authUrl = 'https://ap-southeast-1.aws.services.cloud.mongodb.com/api/client/v2.0/app/data-btthb/auth/providers/custom-function/login';
 
         const requestData = {
-            "username": username,
-            "password": password
+            "user": username,
+            "pass": password
         };
 
-        return new Promise((resolve, reject) => {
-            fetch(authUrl, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(requestData),
+        fetch(authUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestData),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
             })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    console.log('Printing successful JSON Data: ' + JSON.stringify(data));
+            .then(data => {
+                console.log('Printing successful JSON Data: ' + JSON.stringify(data));
 
-                    window.location.href = 'index.html';
+                const accessToken = JSON.parse(JSON.stringify(data)).access_token;
+                // Store the access token in localStorage
+                localStorage.setItem('accessToken', accessToken);
+                
+                window.location.href = 'index.html';
+            })
+            .catch(error => {
+                console.error('There was a problem with your fetch operation:', error);
 
-                })
-                .catch(error => {
-                    console.error('There was a problem with your fetch operation:', error);
-                    
-                    alert('Username or password is incorrect. Please try again.');
-                });
-        });
+                alert('Username or password is incorrect. Please try again.');
+            });
     }
 });
